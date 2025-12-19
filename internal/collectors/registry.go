21e980059035d545e9c1ba340fb8b3eafe5effca
adrelien <adr@adrelien.com>
2025-12-19 21:21:32 +0300

    added macos support

--- internal/collectors/registry.go
+++ internal/collectors/registry.go
@@ -8,7 +8,7 @@ type Collector func(p map[string]string) (map[string]interface{}, error)
 
 func Get(name string) (Collector, error) {
 	switch name {
-	case "linux", "system", "windows":
+	case "linux", "system", "windows", "macos":
 		return SystemCollector, nil
 	case "meshtastic":
 		return MeshtasticCollector, nil
